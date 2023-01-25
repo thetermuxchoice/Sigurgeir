4812677b79b31084de1aa6f561654f6e6e3fdb43
nestordqa <ndqa96@gmail.com>
2023-01-24 22:19:50 -0400

    first section why us already working

--- src/components/WhyUs/WhyUs.jsx
+++ src/components/WhyUs/WhyUs.jsx
@@ -5,7 +5,34 @@ export const WhyUs = () => {
   return (
     <div className='whyContainer' id='whyus'>
         <div className='firstWhy'>
+          <div className='titleWhy'>
+            <div>
+              <h2 className='animated bounce'>Why us?</h2>
+            </div>
+            <div>
+              <p>
+                ThatLifeStudio allows you to search from a wide collection. So you&#39;re sure to find something to
+                your liking. Check out the following reasons why you should choose us:    
+              </p> 
+            </div>           
+          </div>
+          <div className='infoWhy'>
+            <div className='imageWhy'>
 
+            </div>
+            <div className='textWhy'>
+              <div className='hqm'>
+                <h2>High-Quality Materials Used In Our Products</h2>
+              </div>
+              <div className='hqmText'>
+                <p>
+                  High-quality materials are used in all of ThatsLifeStudio&#39;s products. From hoodies to tees, you
+                  can trust that our apparel is made with only the best fabrics and threads. We strive for
+                  perfection, so you don&#39;t have to worry about rips or tears.
+                </p>
+              </div>
+            </div>
+          </div>            
         </div>
         <div className='secondWhy'>
           
